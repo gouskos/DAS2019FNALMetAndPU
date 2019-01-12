168b0718c8d9afec45a3fa019d826996c00d20a5
Loukas <Loukas.Gouskos@cern.ch>
2019-01-12 04:34:28 -0600

    fix file

--- MetAndPu/MetAndPuAnal/python/MetAndPuAnalStep3_cfi.py
+++ MetAndPu/MetAndPuAnal/python/MetAndPuAnalStep3_cfi.py
@@ -1,6 +1,8 @@
 import FWCore.ParameterSet.Config as cms
 
 metandpuanalstep3 = cms.EDAnalyzer('MetAndPuAnalStep3',
+                                   vertices  = cms.InputTag("offlineSlimmedPrimaryVertices"),
                                    mets       = cms.InputTag("slimmedMETs"),
-                                   metFilters = cms.InputTag("TriggerResults"),
+                                   metspuppi  = cms.InputTag("slimmedMETsPuppi"),
+                                   muons     = cms.InputTag("slimmedMuons"),
                                    )
